bdbe5b4cf380f944426fb2137a72bbb154ade101
= <=>
2018-09-11 13:04:26 +0200

    editors

---
+++ editors/physic/__init__.py
@@ -0,0 +1,21 @@
+
+bl_info = {
+    "name": "caca",
+    "author": "BluStroke®",
+    "version": (1, 0),
+    "blender": (2, 78, 0),
+    "location": "",
+    "description": "Building tool for multiplatform release",
+    "warning": "",
+    "wiki_url": "",
+    "category": "System",
+    }
+
+
+
+def register():
+    print("build registered")
+
+def unregister():
+    pass
+    
---
+++ editors/scene/__init__.py
@@ -0,0 +1,9 @@
+from SDK_utils import *
+
+@registerclass
+class SceneEditor:
+    bl_idname = "SDK_scene_editor"
+    bl_name = "Scene Editor"
+
+    def __init__(self):
+        pass
